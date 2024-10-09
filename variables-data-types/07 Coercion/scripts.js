487f2ae77a416ad2840962430f0e195eca3d65de
jnrchry <jnrchry@gmail.com>
2024-10-09 14:48:22 +0000

    hello

---
+++ variables-data-types/07 Coercion/scripts.js
@@ -0,0 +1,8 @@
+// Type coercion
+/* 
+x = 5 + "5"; concartenate
+x = 5 + Number ("5");
+x = 5 * 5; changes string to a number
+
+Add anything to undefined you will get NaN(not a number)
+*/
---
+++ variables-data-types/08 String method/script.js
@@ -0,0 +1,40 @@
+let x;
+
+const name = "John";
+const age = 38;
+
+x = `Hello, my name is ${name} and I am ${age} years old.`;
+
+// String properties and methods
+const s = "Hello World";
+// prperies are more like attributes
+s;
+
+// x = s.length;
+
+// Method
+// Access value by key
+x = s(0);
+
+x = s.toUpperCase();
+x = s.toLowerCase();
+
+x = s.charAt(0);
+x = s.indexOf("H");
+
+x = s.substring(0, 4);
+
+x = s.slice();
+
+z = "  Hello World";
+x = z.trim(z);
+
+x = s.replace("World", "John");
+
+x = s.includes("Hell");
+
+x = s.valueOf();
+
+x = s.split("");
+
+console.log(x);
