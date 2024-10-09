7ac9ec5d2f44eab3405430eea295c4bbab1efcea
jnrchry <jnrchry@gmail.com>
2024-10-09 15:04:58 +0000

    task

---
+++ variables-data-types/09 Challenge/Script.js
@@ -0,0 +1,10 @@
+// change the first letter to uppercase
+myString = "developer";
+
+let myNewString;
+myNewString = myString.charAt(0).toUpperCase() + myString.substring(1);
+myNewString = myString[0].toUpperCase() + myString.substring(1);
+myNewString = myString[0].toUpperCase() + myString.slice(1);
+myNewString = `${myString[0].toUpperCase()}${myString.substring(1)}`;
+
+console.log(myNewString);
