4c25c2717ad8be56529a71a6a7df6d11398c1907
jnrchry <jnrchry@gmail.com>
2024-10-09 14:17:52 +0000

    he

---
+++ variables-data-types/06 Operators/script.js
@@ -0,0 +1,39 @@
+// Operators: symbols we use in expression
+/*
+1. Arithmetic operators
+let x;
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 /5 ;
+x = 5 % 5;
+
+concaternation
+x = "Hello" + "World";
+
+Exponent
+x = 2 ** 3;
+
+Increment 
+x = 1;
+x = x + 1;
+x++
+
+Decrement
+x = 1 - 1;
+x--;
+
+2. Assignemnt operators
+x = 10;
+x += 5; x = x + 5;
+
+3 comparison operators
+x = 2 == 2; test the actual vaule 
+x = 2 === 2; equality; test the vaule and type
+x = 2 != 2, not qual
+x = 2 !== 2, test the vaule and type
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+*/
