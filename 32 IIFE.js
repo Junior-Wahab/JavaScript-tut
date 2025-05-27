5f4d22ee4785790b53498609a59610f7e3a7d039
Junior-Wahab <jnrmwahab@gmail.com>
2025-05-27 21:35:21 +0000

    IIFE

---
+++ 32 IIFE.js
@@ -0,0 +1,12 @@
+// immediately Invoked Function Expression
+(function () {
+  const user = 'John';
+  console.log(user);
+
+  const hello = () => console.log(`Hello from the IIFE`);
+  hello();
+})();
+
+(function (name) {
+  console.log(`Hello ${name}`);
+})('Joe');
